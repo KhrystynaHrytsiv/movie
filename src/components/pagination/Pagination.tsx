a2df3af773d6ff1d37a363fb522beb0d5b37f8ca
KhrystynaHrytsiv <khrystyna.hrytsiv@gmail.com>
2025-11-02 15:54:47 +0200

    changing style

--- src/components/pagination/Pagination.tsx
+++ src/components/pagination/Pagination.tsx
@@ -26,8 +26,8 @@ const Pagination = () => {
 
     return (
         <div style={{justifyContent:'space-between', display:'flex', height: '30px', width:'300px'}}>
-            <button disabled={page===1} onClick={prev} style={{height: '30px', width:'100px',  background:'#a6eaea', color: '#01012c'}}>prev</button>
-            <button disabled={page>=500} onClick={next} style={{height: '30px', width:'100px',  background:'#a6eaea', color: '#01012c'}}>next</button>
+            <button disabled={page===1} onClick={prev} style={{height: '30px', width:'100px',  background:'#1fb9b9', color: '#01012c', borderRadius: '5px', border: 'none'}}>prev</button>
+            <button disabled={page>=500} onClick={next} style={{height: '30px', width:'100px',  background:'#1fb9b9', color: '#01012c', borderRadius: '5px', border: 'none'}}>next</button>
         </div>
     );
 };
